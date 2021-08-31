03a89256ec35dc31800d34d599ce4e6145ecdb7e
viv-garot <vivien@hashicorp.com>
2021-08-31 15:51:59 +0200

    modifed install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 # TFE installation
+cp /vagrant/config/replicated.conf /etc/replicated.conf
 curl -o install.sh https://install.terraform.io/ptfe/stable
 bash ./install.sh \
     no-proxy \
     private-address=192.168.10.10 \
     public-address=192.168.10.10
+usermod -aG docker vagrant
